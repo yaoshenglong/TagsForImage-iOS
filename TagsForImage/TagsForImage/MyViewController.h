1cb47dcea5ecdd7093e72ea1b7a5964e70f57b37
姚胜龙 <shenglong.yao@yi23.net>
2017-02-21 13:16:22 +0800

    init project

---
+++ TagsForImage/TagsForImage/MyViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TagsForImage
+//
+//  Created by 姚胜龙 on 17/2/21.
+//  Copyright © 2017年 姚胜龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+
+@end
+
---
+++ TagsForImage/TagsForImage/MyViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TagsForImage
+//
+//  Created by 姚胜龙 on 17/2/21.
+//  Copyright © 2017年 姚胜龙. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
